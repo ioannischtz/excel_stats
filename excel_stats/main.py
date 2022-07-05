bab3f570cb3f07e44e18066da79b0d264fe5bb19
ioannischtz <ioannischtz@gmail.com>
2022-07-05 13:08:08 +0300

    setup the project folder structure, add description of required tasks in main.py

---
+++ excel_stats/main.py
@@ -0,0 +1,28 @@
+# ---- Open excel sheet ----
+
+# ---- perform statistical analysis ----
+
+# 1 Location
+# 1.1 arithmetic mean
+# 1.2 median
+# 1.3 mode
+# 1.4 interquartile mean
+
+# 2 Spread
+# 2.1 std deviation
+# 2.2 variance
+# 2.3 range
+# 2.4 interquartile range
+# 2.5 absolute deviation
+# 2.6 mean absolute difference
+# 2.7 distance standard deviation
+# 2.8 Gini coefficient
+
+# 3 Shape
+# 3.1 skewness
+
+# 4 Dependence
+# 4.1 Pearson product-moment correlation coefficient
+# 4.2 Spearman's rank coreelatio ncoefficient
+
+# ---- Visualize the results ----
