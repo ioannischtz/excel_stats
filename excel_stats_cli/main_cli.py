e282ac3e65be59008793b9ae42db46cec6916fe0
HjortOfCheliax <g.gkipatoudis@gmail.com>
2022-07-08 19:09:19 +0300

    splitting main to main_lib and main_cli

--- excel_stats_cli/main_cli.py
+++ excel_stats_cli/main_cli.py
@@ -1,4 +1,6 @@
+# ---- Open excel sheet ----
 from openpyxl import load_workbook
+
 
 def extractExceldata_cli():
     wb = load_workbook(filename=input(
@@ -14,18 +16,8 @@ def extractExceldata_cli():
     for i, col_name in enumerate(col_names):
         data_dict[col_name] = [row[i] for row in data]
     print("\nThe excel file contains the following columns:\n", col_names,
-      "\nThis is the whole excel file in a dictionary:\n", data_dict)
+          "\nThis is the whole excel file in a dictionary:\n", data_dict)
 
-def extractExceldata(inputfile):
-    wb = load_workbook(filename=inputfile)
-    sheet = wb.active
-    for names in sheet.iter_rows(min_row=1,
-                                 max_row=1,
-                                 values_only=True):
-        col_names = names
-    data = [[sheet.cell(row=i, column=j).value for j in range(
-            1, sheet.max_column+1)] for i in range(2, sheet.max_row+1)]
-    data_dict = {}
-    for i, col_name in enumerate(col_names):
-        data_dict[col_name] = [row[i] for row in data]
-    return (col_names, data_dict)
+
+# 1 Location
+# 1.1 arithmetic mean
---
+++ excel_stats_lib/main_lib.py
@@ -0,0 +1,21 @@
+# ---- Open excel sheet ----
+from openpyxl import load_workbook
+
+
+def extractExceldata(inputfile):
+    wb = load_workbook(filename=inputfile)
+    sheet = wb.active
+    for names in sheet.iter_rows(min_row=1,
+                                 max_row=1,
+                                 values_only=True):
+        col_names = names
+    data = [[sheet.cell(row=i, column=j).value for j in range(
+            1, sheet.max_column+1)] for i in range(2, sheet.max_row+1)]
+    data_dict = {}
+    for i, col_name in enumerate(col_names):
+        data_dict[col_name] = [row[i] for row in data]
+    return (col_names, data_dict)
+
+
+# 1 Location
+# 1.1 arithmetic mean
