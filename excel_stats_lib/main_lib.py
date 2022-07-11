8c729b6ce5351c9789ca3723bf21d40153c441d5
HjortOfCheliax <g.gkipatoudis@gmail.com>
2022-07-11 13:53:10 +0300

    code for Arithmetic Mean function

--- excel_stats_lib/main_lib.py
+++ excel_stats_lib/main_lib.py
@@ -1,10 +1,14 @@
 # ---- Open excel sheet ----
 from openpyxl import load_workbook
+# File path to test the code: E:\DEV\excel_stats\data\soundnames-LTM coordinates.xlsx
+inputfile = "E:\DEV\excel_stats\data\soundnames-LTM coordinates.xlsx"
 
 
 def extractExceldata(inputfile):
     wb = load_workbook(filename=inputfile)
     sheet = wb.active
+    max_r = sheet.max_row
+    max_c = sheet.max_column
     for names in sheet.iter_rows(min_row=1,
                                  max_row=1,
                                  values_only=True):
@@ -14,8 +18,15 @@ def extractExceldata(inputfile):
     data_dict = {}
     for i, col_name in enumerate(col_names):
         data_dict[col_name] = [row[i] for row in data]
-    return (col_names, data_dict)
+    return (col_names, data_dict, max_r, max_c)
 
 
 # 1 Location
-# 1.1 arithmetic mean
+# 1.1 Arithmetic Mean
+
+
+def arMean_col_i(columnName):
+    col_names, data_dict, max_r, max_c = extractExceldata(inputfile)
+    col_num = col_names.index(columnName)
+    x = sum(data_dict[col_names[col_num]])/(max_r-1)
+    print("The arithmetic mean of column '", columnName, "' is: ", x)
